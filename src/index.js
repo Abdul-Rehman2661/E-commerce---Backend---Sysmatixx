66fd62ab6cceac496527a8a267e280501b0df633
Abdul Rehman <abdulrahmankhan.8x@gmail.com>
2026-06-22 18:53:01 +0500

    Fixed dotenv initialization order for production

--- src/index.js
+++ src/index.js
@@ -1,24 +1,44 @@
+import dotenv from "dotenv";
+dotenv.config();
 import { connectDB } from "./connection/db.js";
 import express from "express";
-import dotenv from "dotenv";
 import routes from "./routes/index.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import paymentRoutes from "./routes/payment.route.js";
 
+
+
 const app = express();
-dotenv.config();
+
 
 app.use("/api/payment/webhook", express.raw({ type: "application/json" }));
 
 app.use(express.json());
 app.use(cookieParser());
+
+const allowedOrigins = [
+  //"http://localhost:5173",                           
+  "https://e-commerce-frontend-sysmatixx.vercel.app"  // Replace with your EXACT live Vercel frontend URL
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps, curl, or Postman)
+      if (!origin) return callback(null, true);
+      
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, 
+  })
 );
+
+
 
 app.use(express.urlencoded({ extended: true }));
 
